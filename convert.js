5f62ea114cd1c4af76e4b6e9e8b201ea7926130c
Johannes Kamleitner <johannes.kamleitner@hotmail.com>
2018-10-25 13:23:20 +0200

    done

--- convert.js
+++ convert.js
@@ -1,1 +1,9 @@
-console.log('Not implemented yet!');
+const converter = require("convert-units");
+if (process.argv.length == 6 && !isNaN(process.argv[2]) && process.argv[4] == "to") {
+    try {
+        let result = converter(process.argv[2]).from(process.argv[3]).to(process.argv[5]);
+        console.log(process.argv[2] + " " + process.argv[3] + " are " + result + " " + process.argv[5]);
+    } catch (e) {
+        console.log("Invalid parameters");
+    }
+} else console.log("Invalid parameters");
